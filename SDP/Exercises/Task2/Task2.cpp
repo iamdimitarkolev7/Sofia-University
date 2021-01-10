4bec5105b46133e43df043b2f3ce869c1d39d62a
Dimitar Kolev <31987284+iamdimitarkolev7@users.noreply.github.com>
2021-01-10 17:24:14 +0200

    Add more tasks

--- SDP/Exercises/Task2/Task2.cpp
+++ SDP/Exercises/Task2/Task2.cpp
@@ -83,16 +83,98 @@ bool is_symmetric_value(const Node<T>* node)
 	return is_symmetric_value_helper(node, node);
 }
 
+template <class T>
+Node<T>* sumTrees(const Node<T>* node1, const Node<T>* node2)
+{
+	if (node1 == nullptr && node2 == nullptr)
+	{
+		return nullptr;
+	}
+	if (node1 != nullptr && node2 == nullptr)
+	{
+		return new Node<T>{ node1->data, sumTrees(node1->left, node2), sumTrees(node1->left, node2)  };
+	}
+	if (node1 == nullptr && node2 != nullptr)
+	{
+		return new Node<T>{ node2->data, sumTrees(node1, node2->left), sumTrees(node1, node2->right) };
+	}
+	if (node1 && node2)
+	{
+		return new Node<T>{ node1->data + node2->data, sumTrees(node1->left, node2->left), sumTrees(node1->right, node2->right) };
+	}
+}
+
+template <class T>
+void print(const Node<T>* node)
+{
+	if (node == nullptr)
+	{
+		return;
+	}
+
+	print(node->left);
+	std::cout << node->data << " ";
+	print(node->right);
+}
+
+void print2DUtil(const Node<int>* root, int space)
+{
+	if (root == nullptr)
+		return;
+ 
+	space += 5;
+	
+	print2DUtil(root->right, space);
+
+	std::cout << std::endl;
+	for (int i = 5; i < space; i++)
+		std::cout << " ";
+	std::cout << root->data << "\n";
+
+	print2DUtil(root->left, space);
+}
+
+void print2D(const Node<int>* root)
+{
+	print2DUtil(root, 0);
+}
+
 int main()
 {
 	const Node<int>* root =
 		new Node<int>{ 1,
 			new Node<int>{2,
-				new Node<int>{4, nullptr, nullptr},
-				new Node<int>{5, nullptr, nullptr}},
+				new Node<int>{3,
+					nullptr,
+					nullptr},
+				new Node<int>{4,
+					nullptr,
+					nullptr}},
 			new Node<int>{ 2,
-				new Node<int>{4, nullptr, nullptr},
-				new Node<int>{5, nullptr, nullptr} }};
+				new Node<int>{4,
+					nullptr,
+					nullptr},
+				new Node<int>{3,
+					nullptr,
+					nullptr}} };
+
+	const Node<int>* anotherRoot =
+		new Node<int>{ 5,
+			new Node<int>{4,
+				nullptr,
+				new Node<int>{3,
+					nullptr,
+					nullptr}},
+			new Node<int>{8,
+				new Node<int>{6,
+					nullptr,
+					nullptr},
+				nullptr}};
+
+	const Node<int>* resultRoot = sumTrees<int>(root, anotherRoot);
+
+	print2D(resultRoot);
+	std::cout << std::endl;
 
 	const std::pair<int, int> result = std::make_pair(sum_even<int>(root, 0), product_odd<int>(root, 0));
 
